e921a6d032fbd493bf04ad84c9137f7b8dfa58b5
Ajinkya Pawar <115565449+ajinkyamspawar21@users.noreply.github.com>
2023-04-15 18:58:21 +0530

    Add files via upload

---
+++ Python_SelfStudy_colorQRgeneration.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 12 19:45:32 2023
+
+@author: AJINKYA
+"""
+
+import qrcode
+import PIL
+from PIL import Image
+
+qr = qrcode.QRCode(version = 1, error_correction = qrcode.constants.ERROR_CORRECT_H, box_size = 20, border = 8)
+
+qr.add_data("https://www.youtube.com/@seedingyounger2468")
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="pink", back_color = "white")
+img.save("color@SeedingYounger.png")
