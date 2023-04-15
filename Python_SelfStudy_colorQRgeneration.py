68293843faf1700bd816e99ffab03a00ab900bb5
Ajinkya Pawar <115565449+ajinkyamspawar21@users.noreply.github.com>
2023-04-15 18:59:53 +0530

    Update Python_SelfStudy_colorQRgeneration.py

--- Python_SelfStudy_colorQRgeneration.py
+++ Python_SelfStudy_colorQRgeneration.py
@@ -11,8 +11,8 @@ from PIL import Image
 
 qr = qrcode.QRCode(version = 1, error_correction = qrcode.constants.ERROR_CORRECT_H, box_size = 20, border = 8)
 
-qr.add_data("https://www.youtube.com/@seedingyounger2468")
+qr.add_data("linkofwebsite")
 qr.make(fit=True)
 
 img = qr.make_image(fill_color="pink", back_color = "white")
-img.save("color@SeedingYounger.png")
+img.save("nameforimage.png")
